170b330b1f35c3966766535a357d8d0f47c190a1
Gabriel Alves <l.luis.gabriel@outlook.com>
2025-05-31 10:49:00 -0300

    refactor(app): add application ping route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApplicationBootstrapOptions } from './common/interfaces/application-bootstrap-options.interface';
 import { CoreModule } from './core/core.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   static register(options: ApplicationBootstrapOptions) {
